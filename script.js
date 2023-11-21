10043ee241872555b8046089bd5e7602aa743f82
Adam Welin <welinadam@users.noreply.github.com>
2023-11-21 14:45:36 +0100

    Added media query script

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ langOptions.forEach((langOption) => {
     }, 300);
   });
 });
+
+// Media query
+function myFunction(x) {
+  const story = document.querySelector(".story");
+  story.innerHTML = "";
+  if (x.matches) {
+    // If media query matches
+    const paragraphOne = document.createElement("p");
+    paragraphOne.innerHTML =
+      "TrailBlaze är kollektionen för dig som inte backar för utmaningar. MedProTechs vision är att skapa en meningsfull och berikande upplevelse för sina kunder genom att erbjuda högkvalitativa vildmarksprodukter som förbättrar deras utomhuslivsstil. Vi strävar efter att vara en pålitlig partner för våra kunder när det gäller utrustning och teknik för äventyr i naturen.";
+    const paragraphTwo = document.createElement("p");
+    paragraphTwo.innerHTML =
+      "Vi fokuserar mest på att erbjuda produkter som inte bara uppfyller, utan överträffar sina kunders förväntningar när det gäller kvalitet och funktionalitet. Vårt sortiment inkluderar väl genomtänkta produkter, som powerbanks och vattentäta hörlurar, som är utformade för att möta de specifika behoven hos dem som älskar att spendera tid i naturen.";
+    story.appendChild(paragraphOne);
+    story.appendChild(paragraphTwo);
+  } else {
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML =
+      "TrailBlaze är kollektionen för dig som inte backar för utmaningar. Med...";
+    story.appendChild(paragraph);
+  }
+}
+
+var x = window.matchMedia("(min-width: 450px)");
+myFunction(x); // Call listener function at run time

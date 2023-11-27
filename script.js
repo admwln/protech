a627a14b3a6603474c57425bf7c59f11a1a5acf4
Adam Welin <welinadam@users.noreply.github.com>
2023-11-27 15:55:44 +0100

    Primary and secondary images and hover effect for products added, working on deskt

--- script.js
+++ script.js
@@ -41,28 +41,55 @@ function myFunction(x) {
 
     // Desktop images
     const productSmallUrls = [
-      "/images/placeholders/image-placeholder-1.png", //Jacket BLACK
-      "/images/placeholders/image-placeholder-2.png", //Jacket BLUE
-      "/images/placeholders/image-placeholder-3.png", //Jacket YELLOW
+      "/images/watch_pro_1.jpg", // Watch pro
+      "/images/powerbank_1.jpg", // Powerbank
+      "/images/jacket_basic_1.jpg", // Jacket basic
+      "/images/watch_pro_2.jpg", // Watch pro 2
+      "/images/powerbank_2.jpg", // Powerbank 2
+      "/images/jacket_basic_2.jpg", // Jacket basic 2
     ];
 
-    const productsSmallImgs = document.querySelectorAll(".products.small img");
+    const productsSmallImgs = document.querySelectorAll(
+      ".products.small img.primary"
+    );
     productsSmallImgs.forEach((img, index) => {
       img.src = productSmallUrls[index];
     });
 
+    const productsSmallImgsSecondary = document.querySelectorAll(
+      ".products.small img.secondary"
+    );
+    productsSmallImgsSecondary.forEach((img, index) => {
+      img.src = productSmallUrls[index + 3];
+    });
+
     const productLargeUrls = [
-      "/images/placeholders/image-placeholder-4.png", //Jacket GREEN
-      "/images/placeholders/image-placeholder-8.png", //Jacket brown
-      "/images/placeholders/image-placeholder-5.png", //Spark
-      "/images/placeholders/image-placeholder-6.png", //Power bank
-      "/images/placeholders/image-placeholder-3.png", //Jacket YELLOW
-      "/images/placeholders/image-placeholder-9.png", //Powerbank pro
+      "/images/powerbank_1.jpg", // Powerbank
+      "/images/jacket_basic_1.jpg", // Jacket basic
+      "/images/watch_basic_1.jpg", // Watch basic
+      "/images/jacket_pro_1.jpg", // Jacket pro
+      "/images/watch_pro_1.jpg", // Watch pro
+      "/images/powerbank_pro_1.jpg", //Powerbank pro
+      "/images/powerbank_2.jpg", // Powerbank
+      "/images/jacket_basic_2.jpg", // Jacket basic
+      "/images/watch_basic_2.jpg", // Watch basic
+      "/images/jacket_pro_2.jpg", // Jacket pro
+      "/images/watch_pro_2.jpg", // Watch pro
+      "/images/powerbank_pro_2.jpg", //Powerbank pro
     ];
 
-    const productsLargeImgs = document.querySelectorAll(".products.large img");
+    const productsLargeImgs = document.querySelectorAll(
+      ".products.large img.primary"
+    );
     productsLargeImgs.forEach((img, index) => {
       img.src = productLargeUrls[index];
+    });
+
+    const productsLargeImgsSecondary = document.querySelectorAll(
+      ".products.large img.secondary"
+    );
+    productsLargeImgsSecondary.forEach((img, index) => {
+      img.src = productLargeUrls[index + 6];
     });
 
     const onceUponImgSmall = document.querySelector(".once-upon-img-top > img");
@@ -124,7 +151,8 @@ function showOnceUponText() {
   newSection.classList.add("once-upon-onclick-container");
 
   const newDiv = document.createElement("div");
-  newDiv.innerHTML = "<h2>Utrustad för utmaning</h2><p>Utforska vildmarken med ProTechs senaste tillskott - Sarek X4 Series Jacket. Utrustad med avancerad teknologi, inklusive elslingor som ger långvarig värme, och ett 100% vattentätt yttre, håller denna jacka dig bekväm och skyddad oavsett väder. Dess mångsidiga design passar perfekt för alla utomhusaktiviteter, med fokus på både funktionalitet och stil. <br><br> Hög kvalitet och hållbarhet är kännetecken för denna jacka, vilket gör den till det ultimata valet för äventyrsentusiaster. Upptäck naturen med självsäkerhet i ProTechs Sarek X4 Series Jacket.</p>";
+  newDiv.innerHTML =
+    "<h2>Utrustad för utmaning</h2><p>Utforska vildmarken med ProTechs senaste tillskott - Sarek X4 Series Jacket. Utrustad med avancerad teknologi, inklusive elslingor som ger långvarig värme, och ett 100% vattentätt yttre, håller denna jacka dig bekväm och skyddad oavsett väder. Dess mångsidiga design passar perfekt för alla utomhusaktiviteter, med fokus på både funktionalitet och stil. <br><br> Hög kvalitet och hållbarhet är kännetecken för denna jacka, vilket gör den till det ultimata valet för äventyrsentusiaster. Upptäck naturen med självsäkerhet i ProTechs Sarek X4 Series Jacket.</p>";
 
   newSection.appendChild(newDiv);
 
